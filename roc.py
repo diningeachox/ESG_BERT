9ae7505f73d2501425636374bb09b35eb59e1bd0
Jack Ding <jackxiaochending@gmail.com>
2019-10-15 17:15:54 -0400

    Updated roc curve calculations for each class

--- roc.py
+++ roc.py
@@ -18,7 +18,7 @@ from sklearn.metrics import roc_auc_score, accuracy_score
 from matplotlib import pyplot
 
 
-np.set_printoptions(suppress=True)
+#np.set_printoptions(suppress=True)
 # Initialize session
 sess = tf.Session()
 bert_path = "https://tfhub.dev/google/bert_uncased_L-12_H-768_A-12/1"
@@ -26,12 +26,17 @@ max_seq_length = 128
 # Instantiate variables
 bml.initialize_vars(sess)
 #Load custom layers such as BertLayer
-model = tf.keras.models.load_model("least_loss_model_d05_lr=3_ft=0.h5",
+model = tf.keras.models.load_model("least_loss_model_two-stage.h5",
 custom_objects={'BertLayer': bml.BertLayer, 'precision_m': bml.precision_m,
 'recall_m': bml.recall_m})
 #model = load_model("bert_model.h5", custom_objects={'BertLayer': bml.BertLayer})
 
 print("BERT model succesfully loaded.")
+
+#Single category metrics
+
+
+
 
 #Take datasets to be the tweets csv
 train_df, test_df = bml.load_datasets_csv("modified_tweets.csv")
@@ -59,6 +64,36 @@ examples = bml.convert_text_to_examples(test_text, test_label)
 
 pred = model.predict([test_input_ids, test_input_masks, test_segment_ids])
 
+
+#FPR-TPR curves for each class
+x = np.arange(-0.5,0.51,0.01) #x ranges from 0 to 1 in increments of 0.05
+for y in range(0, 13):
+    print("{}-th class".format(y+1))
+    #pre = [K.eval(bml.precision_single(test_labels, pred, val, y)) for val in x]
+    rec = [K.eval(bml.recall_single(test_labels, pred, val, y)) for val in x]
+    fpr = [K.eval(bml.fpr_single(test_labels, pred, val, y)) for val in x]
+
+    # plot ROC curve
+    pyplot.plot(fpr, rec, marker='.', label="ROC curve")
+    #x = y line
+    pyplot.plot([0,1], [0,1], linestyle='--')
+    # Add X and y Label
+    pyplot.xlabel('FPR')
+    pyplot.ylabel('TPR')
+
+    # Add a grid
+    pyplot.grid(alpha=.4,linestyle='--')
+
+    # Add a Legend
+    pyplot.legend()
+    #Save the graph to a pgn file
+    pyplot.savefig("curves/class{}.png".format(y+1))
+    print("Saved roc curve for class {}\n".format(y+1))
+    #Clear plot for next graph
+    pyplot.clf()
+
+
+'''
 #FPR-TPR curves
 x = np.arange(-0.5,0.51,0.01) #x ranges from 0 to 1 in increments of 0.05
 pre = [K.eval(bml.precision_m(test_labels, pred, val)) for val in x]
@@ -66,6 +101,7 @@ rec = [K.eval(bml.recall_m(test_labels, pred, val)) for val in x]
 fpr = [K.eval(bml.fpr_m(test_labels, pred, val)) for val in x]
 F1 = [2 * (x * y) / (x + y + K.epsilon()) for x, y in zip(pre, rec)]
 one_minus_fpr = [1 - K.eval(bml.fpr_m(test_labels, pred, val)) for val in x]
+'''
 
 '''
 # plot ROC curve
@@ -77,6 +113,7 @@ pyplot.xlabel('FPR')
 pyplot.ylabel('TPR')
 '''
 
+'''
 #Plot TPR vs 1 - FPR
 pyplot.plot(x, tpr, marker='.', label="TPR")
 #pyplot.plot(x, one_minus_fpr, marker='.', label="1 - FPR")
@@ -92,3 +129,4 @@ pyplot.grid(alpha=.4,linestyle='--')
 pyplot.legend()
 
 pyplot.show()
+'''
