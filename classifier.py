c48c6fa53863cbbf9ffc9de0e095589cf29fbef8
Jack Ding <jackxiaochending@gmail.com>
2019-11-29 19:06:43 -0500

    Printed scored data

--- classifier.py
+++ classifier.py
@@ -15,6 +15,9 @@ from keras.initializers import glorot_uniform
 import csv
 from keras import backend
 
+os.environ['TF_KERAS'] = '1' #environment variable for RAdam
+from keras_radam import RAdam
+
 np.set_printoptions(suppress=True)
 # Initialize session
 sess = tf.Session()
@@ -23,22 +26,19 @@ max_seq_length = 128
 # Instantiate variables
 bml.initialize_vars(sess)
 #Load custom layers such as BertLayer
-model = tf.keras.models.load_model("least_loss_model_d05_lr=3_ft=0.h5",
-custom_objects={'BertLayer': bml.BertLayer})
+model = tf.keras.models.load_model("2stage_model.h5",
+custom_objects={'BertLayer': bml.BertLayer, 'precision_m': bml.precision_m,
+'recall_m': bml.recall_m, 'RAdam': RAdam})
 #model = load_model("bert_model.h5", custom_objects={'BertLayer': bml.BertLayer})
 
 print("BERT model succesfully loaded.")
-
-tweets_text = [" ".join(t.split()[0:max_seq_length]) for t in tweets]
-tweets_text = np.array(tweets_text, dtype=object)[:, np.newaxis]
-
 #Take datasets to be the tweets csv
 train_df, test_df = bml.load_datasets_csv("modified_tweets.csv")
 
 test_text = test_df["tweet_content"].tolist()
 test_text = [" ".join(t.split()[0:max_seq_length]) for t in test_text]
 test_text = np.array(test_text, dtype=object)[:, np.newaxis]
-test_label = test_df.iloc[:, 6:-1].values.tolist()
+test_label = test_df.iloc[:, 7:-1].values.tolist()
 
 # Instantiate tokenizer
 tokenizer = bml.create_tokenizer_from_hub_module(bert_path)
@@ -59,11 +59,9 @@ pred = model.predict([test_input_ids, test_input_masks, test_segment_ids])
 
 #Write sample predictions to predictions.csv
 with open('predictions.csv', mode='w') as file:
-    file_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    file_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, lineterminator = '\n')
     #Column names
-    file_writer.writerow(["Actual/Predicted",
-    "Corporate Behaviour - Business Ethics",
-    "Corporate Behaviour - Business Ethics",
+    file_writer.writerow(["Corporate Behaviour - Business Ethics",
     "Corporate Behaviour - Anti-Competitive Practices",
     "Corporate Behaviour - Corruption & Instability",
     "Privacy & Data Security",
@@ -76,13 +74,9 @@ with open('predictions.csv', mode='w') as file:
     "Positive",
     "Negative",
     "Tied to Specific Company(Y/N)"])
-    for i in range(100):
+    for i in range(len(test_label)):
         #Make 25 sample prediction using the trained model from test data
         pred = model.predict([test_input_ids[i:i+1], test_input_masks[i:i+1], test_segment_ids[i:i+1]])
-        #pred = map(lambda x: np.round(x + 0.455), pred) #Optimal threshold is 0.045
-        pred = map(lambda x: np.round(x + 0.25), pred) #Best F1 score at 0.25
-        #pred_string = (", ".join(str(p) for p in pred))
-        file_writer.writerow(pred)
-        #label_string = (", ".join(str(p) for p in test_labels[i]))
+        #Round to 8 decimal places
+        file_writer.writerow(list(np.around(np.array(pred[0]),8)))
         file_writer.writerow(test_labels[i]) #Actual labels
-        file_writer.writerow("--------------------------------------------")
